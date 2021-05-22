6e36825b41d544d9a78167bb75815dc47bafdf47
Varen Aggarwal <varenaggarwal@gmail.com>
2021-05-22 13:16:29 +0530

    remove comments

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,18 +1,3 @@
-// import { Button, ButtonGroup } from "@chakra-ui/react";
-// import { Link } from "react-router-dom";
-
-// export function Homepage() {
-//   return (
-//     <div>
-//       <h1>HomePage</h1>
-//       <h1>Learn the champions of Tennis</h1>
-//       <Link to="/quiz">
-//         <Button colorScheme="green">Start Quiz</Button>
-//       </Link>
-//     </div>
-//   );
-// }
-
 import {
   Flex,
   Container,
